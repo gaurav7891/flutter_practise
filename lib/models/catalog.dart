6ff9f3734260713b82dad1b71086cd1a55305c48
gauravkumar <gauravkumar.mishra@pnp-hcl.com>
2021-11-13 16:03:18 +0530

    day 9 completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,13 @@
+class Item {
+  String id;
+  String name;
+  String desc;
+  num price;
+  String color;
+  String image;
+
+  Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
+
+}
+
+final product = [(Item(id: "1234", name:"asdasd", desc: "adasd", price:123, color: "#fff000d", image: "asdasd"))];
