cceb7330bde71b6173cef42e0d01f34969731910
gauravkumar <gauravkumar.mishra@pnp-hcl.com>
2021-11-13 18:25:47 +0530

    day 10 compeleted

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:day_1/widgets/themes.dart';
 import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
 import 'pages/login_page.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,6 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     doSomething();
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: MyTheme.lightTheme(context),
       darkTheme: MyTheme.darkTheme(context),
       themeMode: ThemeMode.light,
