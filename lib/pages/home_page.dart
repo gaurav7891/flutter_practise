d359ea001cfe6518e8b66dac48987e302f2997a1
gauravkumar <gauravkumar.mishra@pnp-hcl.com>
2021-11-15 14:17:47 +0530

    day14 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -51,13 +51,39 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
-            ? ListView.builder(
-                itemCount: CatalogModel.items!.length,
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 16.0,
+                    crossAxisSpacing: 16.0),
                 itemBuilder: (context, index) {
-                  return ItemWidget(
-                    item: CatalogModel.items![index],
-                  );
+                  final item = CatalogModel.items![index];
+                  return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12)),
+                      child: GridTile(
+                          header: Container(
+                            child: Text(
+                              item.name,
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            padding: const EdgeInsets.all(12),
+                            decoration: BoxDecoration(color: Colors.deepPurple),
+                          ),
+                          footer: Container(
+                            child: Text(
+                              item.price.toString(),
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            padding: const EdgeInsets.all(12),
+                            decoration: BoxDecoration(color: Colors.black),
+                          ),
+                          child: Image.network(
+                            item.image,
+                          )));
                 },
+                itemCount: CatalogModel.items!.length,
               )
             : Center(
                 child: CircularProgressIndicator(),
