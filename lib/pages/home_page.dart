b59f85092c7759738183cea739cae66f30e3306c
gauravkumar <gauravkumar.mishra@pnp-hcl.com>
2021-11-25 08:01:22 +0530

    day28 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:day_1/core/store.dart';
+import 'package:day_1/models/cart.dart';
 import 'package:day_1/models/catalog.dart';
 import 'package:day_1/utils/routes.dart';
 import 'package:day_1/widgets/home_widgets/catalog_header.dart';
@@ -7,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:convert';
 import 'package:velocity_x/velocity_x.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -16,6 +19,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final url = "https://api.jsonbin.io/b/619eefa762ed886f91544be8";
+  var secretKey =
+      "\$2b\$10\$J.pTkvG4ZdqJOp4jQNslq.5g18bntr/gf2by0zL3FX7YrypbdLOhy";
   @override
   void initState() {
     super.initState();
@@ -24,7 +30,10 @@ class _HomePageState extends State<HomePage> {
 
   loadData() async {
     await Future.delayed(Duration(seconds: 2));
-    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    //var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var client = http.Client();
+    var response = await  client.get(Uri.parse(url), headers: {"secret-key": secretKey});
+    var catalogJson = response.body;
     var decodedData = jsonDecode(catalogJson);
     var productsData = decodedData["products"];
     CatalogModel.items = List.from(productsData)
@@ -36,23 +45,24 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final double days = 10;
-    String name = "Flutter";
-    bool _isMale = false;
-    num temp = 30.5; // takes both integer and double
-    var day = 'Tuesday';
-    const pi = 3.14; // value cannot be modified
-    final x = 12; // data can be modified
-
+    final _cart = (VxState.store as MyStore).cartModel;
     return Scaffold(
         backgroundColor: context.theme.backgroundColor,
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => Navigator.pushNamed(context, MyRoutes.cartRoute),
-          backgroundColor: context.theme.buttonColor,
-          child: const Icon(
-            CupertinoIcons.cart,
-            color: Colors.white,
-          ),
+        floatingActionButton: VxBuilder(
+          mutations: {AddMutation, RemoveMutation},
+          builder: (context, _, __) => FloatingActionButton(
+            onPressed: () => Navigator.pushNamed(context, MyRoutes.cartRoute),
+            backgroundColor: context.theme.buttonColor,
+            child: const Icon(
+              CupertinoIcons.cart,
+              color: Colors.white,
+            ),
+          ).badge(
+              color: Vx.red500,
+              size: 22,
+              count: _cart.items.length,
+              textStyle:
+                  TextStyle(fontWeight: FontWeight.bold, color: Colors.white)),
         ),
         body: SafeArea(
           child: Container(
